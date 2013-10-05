5e0a1ff927effdbaad836c2fc23e02ecce25aea4
Hiroshi Saito <h_saito@everlifegroup.com>
2013-10-05 12:13:35 +0900

    baffer移動キーバインドの設定 ファイル情報表示の定義追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,6 +32,9 @@
 ;; 起動時、Welcome to GNU Emacs画面非表示
 (setq inhibit-startup-message t)
 
+;; バッファ切り替え
+(global-set-key "\C-t" 'other-window)
+
 ;; タブ, 全角スペース, 行末空白表示
 (defface my-face-b-1 '((t (:background "NavajoWhite4"))) nil) ; 全角スペース
 (defface my-face-b-2 '((t (:background "gray10"))) nil)	      ; タブ
@@ -104,6 +107,34 @@
                (throw 'end-flag t))))))
 )
 (global-set-key "\C-c\C-r" 'my-window-resizer)
+
+;====================================
+;; File-Info - ファイル情報
+;====================================
+(defun file-info () "
+カレントバッファのファイル情報を表示する。"
+  (interactive)
+  (if (buffer-file-name (current-buffer))
+      (progn
+        (let* ((file-name (buffer-file-name (current-buffer)))
+               (f-attr (file-attributes file-name))
+               (a-time (nth 4 f-attr))  ; 最終アクセス時刻
+               (m-time (nth 5 f-attr))  ; 最終修正時刻
+               (f-size (nth 7 f-attr))  ; ファイルサイズ
+               (f-mode (nth 8 f-attr))  ; ファイル属性
+               (mes1 (format "ファイルパス:   %s\n" file-name))
+               (mes2 (format "最終参照時刻:   %s\n"
+                              (format-time-string
+                               "%Y-%m-%d %H:%M:%S" a-time)))
+               (mes3 (format "最終修正時刻:   %s\n"
+                              (format-time-string
+                               "%Y-%m-%d %H:%M:%S" m-time)))
+               (mes4 (format "ファイルサイズ: %s byte\n" f-size))
+               (mes5 (format "ファイル属性:   %s" f-mode))
+               (mess (concat mes1 mes2 mes3 mes4 mes5)))
+          (message "%s" mess)))
+    nil ))
+
 
 ;====================================
 ; Emacs Lisp パッケージ追加
@@ -278,7 +309,7 @@
   (setq display-buffer-function 'popwin:display-buffer)
   (push '(direx:direx-mode :position left :width 40 :dedicated t) popwin:special-display-config)
   (push '("*Google Translate*" :position bottom :dedicated t) popwin:special-display-config)
-  (push '("*Completions*" :position right :dedicated t) popwin:special-display-config)
+  ;; (push '("*Completions*" :position bottom :dedicated t) popwin:special-display-config)
 )
 
 ;; ;; 起動時に2分割
